1cf8517abdda91dcc7f1751d75562246eabba96f
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-02-15 15:06:08 +0900

    [Edit] <카메라 뷰 수정>
    - 카메라 뷰 수정
    - 캐릭터 오브젝트 변경

--- Assets/script/cameraController.cs
+++ Assets/script/cameraController.cs
@@ -6,16 +6,15 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float cameraSpeed;
-    public float cameraHeight;
-    public float cameraSpace;
 
     public GameObject player;
+    public float playerHeight;
 
     private void Update()
     {
         Vector3 dir = player.transform.position - this.transform.position;
         float deltaSpeed = cameraSpeed * Time.deltaTime;
-        Vector3 moveVector = new Vector3(dir.x * deltaSpeed, (dir.y + cameraHeight) * deltaSpeed, (dir.z - cameraSpace) * deltaSpeed);
+        Vector3 moveVector = new Vector3(dir.x * deltaSpeed, (dir.y + playerHeight) * deltaSpeed, dir.z * deltaSpeed);
         this.transform.Translate(moveVector);
     }
 }
