6a21fc7b6cdc042ea39b258bdb34a71f14cdcd7a
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-02-27 00:43:55 +0900

    feat :: Sprint 추가

--- Assets/script/PlayerMovement.cs
+++ Assets/script/PlayerMovement.cs
@@ -4,36 +4,75 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
-    public float moveSpeed;
-    
+    private float moveSpeed;
+
+    public float walkSpeed;
+    public float sprintSpeed;
+
     public float groundDrag;
 
     public float JumpForce;
     public float jumpCooldown;
     public float airMultiplier;
+
     bool readyToJump;
-    public KeyCode jumpKey;
+    bool isJump;
+    bool isSprint;
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
+
     bool grounded;
 
     public Transform orientation;
 
     Vector3 moveDirection;
     Vector2 Input;
+
     Rigidbody rb;
 
+    public MovementState state;
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        air
+    }
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         readyToJump = true;
+        isSprint = false;
+        isJump = false;
+    }
+    private void OnMove(InputValue value)
+    {
+        Input = value.Get<Vector2>();
+
+    }
+    private void OnJumpStart()
+    {
+        isJump = true;
+    }
+    private void OnJumpStop()
+    {
+        isJump = false;
+    }
+    private void OnSprintStart()
+    {
+        isSprint = true;
+    }
+    private void OnSprintStop()
+    {
+        isSprint = false;
     }
     private void Update()
     {
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
         SpeedControl();
+        JumpControl();
+        StateHandler();
         if (grounded)
             rb.linearDamping = groundDrag;
         else
@@ -45,21 +84,6 @@ public class PlayerMovement : MonoBehaviour
     {
         MovePlayer();
     }
-    private void OnMove(InputValue value)
-    {
-        Input = value.Get<Vector2>();
-
-    }
-    private void OnJump()
-    {
-        Debug.Log(readyToJump);
-        if (Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround) && readyToJump)
-        {
-            readyToJump = false;
-            Jump();
-            Invoke(nameof(ResetJump), jumpCooldown);
-        }
-    }
     private void MovePlayer()
     {
         moveDirection = orientation.forward * Input.y + orientation.right * Input.x;
@@ -69,6 +93,23 @@ public class PlayerMovement : MonoBehaviour
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
 
     }
+    private void StateHandler()
+    {
+        if(grounded && isSprint)
+        {
+            state = MovementState.sprinting;
+            moveSpeed = sprintSpeed;
+        }
+        else if (grounded)
+        {
+            state = MovementState.walking;
+            moveSpeed = walkSpeed;
+        }
+        else
+        {
+            state = MovementState.air;
+        }
+    }
     private void SpeedControl()
     {
         Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
@@ -77,6 +118,15 @@ public class PlayerMovement : MonoBehaviour
         {
             Vector3 limitedVel = flatVel.normalized * moveSpeed;
             rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+        }
+    }
+    private void JumpControl()
+    {
+        if (isJump && grounded && readyToJump)
+        {
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
         }
     }
     private void Jump()
